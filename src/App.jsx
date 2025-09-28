034689f8aab4e97ba7be8e1f3ed338e9c0008a31
mukhammedaliametov <ametovmukhammedali07@gmail.com>
2025-09-28 18:35:50 +0500

    feat: header and hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 
 const App = () => {
   return (
-    <div>
-      <h1 className='underline text-center text-3xl font-bold'>hello world</h1>
-    </div>
+  <>
+  <Navbar />
+  <Hero />
+  </>
   );
 };
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { FaPhone } from "react-icons/fa6";
+import Logo from "../assets/logo.png";
+import { IoMenu, IoClose } from "react-icons/io5";
+
+const Navbar = () => {
+
+    const [nav, setNav] = useState();
+    const handleClickc = () => setNav(!nav);
+
+  const navItems = [
+    { name: "Курорт", link: "#" },
+    { name: "Проживание", link: "#" },
+    { name: "Услуги и тарифы", link: "#" },
+    { name: "Афиша", link: "#" },
+    { name: "Для бизнеса", link: "#" },
+    { name: "Купить онлайн", link: "#" },
+  ];
+  return (
+    <div className="relative max-w-[1260px] mx-auto px-[16px] xl:px-0 font-inter shadow-md sm:shadow-none bg-white z-999">
+      <div className="hidden sm:flex items-center justify-end text-[#B0B0B0] text-[14px] pt-[6px] gap-[12px]">
+        <p>Схема проезда</p>
+        <a href="tel: +79227288288" className="flex items-center gap-[5px]">
+          <FaPhone />
+          +79227288288
+        </a>
+        <a href="tel: +73513790979" className="flex items-center gap-[5px]">
+          <FaPhone />
+          +73513790979
+        </a>
+      </div>
+      <div className="flex items-center justify-between mt-[15px] sm:mt-[-10px] pb-[10px]">
+        <a href="#">
+          <img src={Logo} alt="logo" className="w-[160px] sm:w-[220px]" />
+        </a>
+        <nav className="hidden lg:flex items-center gap-[30px]">
+          {navItems.map((itm, idx) => {
+            return <a key={idx} href={itm.link}>{itm.name}</a>;
+          })}
+        </nav>
+        <div onClick={handleClickc} className="block lg:hidden text-[30px]">
+            {nav ? <IoClose /> : <IoMenu />}
+        </div>
+        <nav className={`absolute top-[80px] sm:top-[95px] flex flex-col gap-[20px] bg-white w-full p-[20px] text-[18px] transition-all duration-300 ${nav ? 'left-0' : 'left-[-100%]'}`}>
+          {navItems.map((itm, idx) => {
+            return <a key={idx} href={itm.link}>{itm.name}</a>;
+          })}
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
